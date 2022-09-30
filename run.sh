9dbd5caee55a60443183bc41b99d511e98b798af
John Duimovich <jduimovich@gmail.com>
2022-09-30 12:12:46 -0400

    logs

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 
+echo before 
+rm -rf output
+mkdir output
 ls output 
 
 docker build  -t jduimovich/demo .
-rm -rf output
-mkdir output
 
 docker run -v output:/mydirectory jduimovich/demo
 
+echo after 
 ls output 
 
